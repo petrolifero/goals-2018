7656c8c2a2a2f5e6d3fc763ec8a9c502fb92fb1b
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2019-10-22 01:58:00 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-bash src/main/test-mr.sh
+function test1()
+{
+    export GOPATH=`pwd`
+    cd "$GOPATH/src/mapreduce"
+    go test -run Sequential mapreduce/ ...
+    cd -
+}
+
+function test2()
+{
+    echo try
+}
+
+function test3()
+{
+    echo try
+}
+
+function test4()
+{
+    echo try
+}
+
+function test5()
+{
+    echo try
+}
+
+
+
+
+for i in `seq 5`
+do
+	test$i
+done
